0c7eecc3ab7dd48f4d4417219765306819a3c69f
Raquel Romo <rqlrom@gmail.com>
2018-06-29 18:25:21 +0200

    hasta paso 3 chachi

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-console.log('>> Ready :)');
+// Función que genera un número aleatorio hasta un máximo dado
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+// Anda, chacho, píntame en la consola un número random hasta 100;
+// console.log('> ' + getRandomNumber(100));
+
+var numAleat = getRandomNumber(100);
+
+console.log('el numero aleatorio es: ' + numAleat);
+
+var buttonJuego = document.querySelector('.button-juego');
+var input = document.querySelector('.entrada-num-juego');
+
+
+function showInput() {
+  console.log('el numero en input es: ' + input.value);
+}
+
+buttonJuego.addEventListener('click', showInput);
+
+
+  if (numAleat === input.value) {
+
+  }
